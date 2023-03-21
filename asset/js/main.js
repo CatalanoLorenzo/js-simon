595c6d6e3fde5a8e20f71647ecadf6debe862f93
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-21 13:30:40 +0100

    initial elemnt dom in js

---
+++ asset/js/main.js
@@ -0,0 +1,4 @@
+const h1El = document.querySelector('h1')
+const textInputValue = document.querySelector('input').value
+const playButton = document.querySelector('play')
+const sendButton = document.querySelector('send')
